d9bbdb7c4a3512c3deab4ee6d08083a45b22ccc8
suryo <suryoatm@gmail.com>
2022-07-19 10:57:23 +0700

    modified : spalshscreen into login screen

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -11,7 +11,7 @@ class SplashScreen extends Component {
     // console.log('component did mount gaes');
     setTimeout(() => {
       //   this.props.navigation.navigate('Home');
-      this.props.navigation.dispatch(StackActions.replace('Home'));
+      this.props.navigation.dispatch(StackActions.replace('LoginLanding'));
     }, 2500);
   };
 

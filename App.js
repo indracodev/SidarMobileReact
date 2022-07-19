63e2c3870af62d035b0f2ec25845cf09bb082dfe
suryo <suryoatm@gmail.com>
2022-07-19 10:59:12 +0700

    modified : routing all login modules

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
 
+import LoginLanding from './src/screens/LoginLanding';
+import SignIn from './src/screens/SignIn';
+import Register from './src/screens/Register';
+import LupaPassword from './src/screens/LupaPassword';
 import Home from './src/screens/Home';
 import Detail from './src/screens/Detail';
 import SplashScreen from './src/screens/SplashScreen';
@@ -14,7 +18,11 @@ function App() {
         initialRouteName="SplashScreen" //memberikan inisial route mana yang pertama kali dipilih
         screenOptions={{headerShown: false}} //mematika header
       >
-        {/* membuat routing home */}
+        {/* membuat routing*/}
+        <Stack.Screen name="LoginLanding" component={LoginLanding} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="LupaPassword" component={LupaPassword} />
         <Stack.Screen name="Home" component={Home} />
         <Stack.Screen name="Detail" component={Detail} />
         <Stack.Screen name="SplashScreen" component={SplashScreen} />

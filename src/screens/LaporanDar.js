6dda932341466cbb3d312432cef446b2c4f4893d
suryo <suryoatm@gmail.com>
2022-07-24 20:07:12 +0700

    modified : laporandar screen connect to axios

--- src/screens/LaporanDar.js
+++ src/screens/LaporanDar.js
@@ -2,6 +2,10 @@ import React, {Component} from 'react';
 import {Text, View, TouchableOpacity, StatusBar, FlatList} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 
+import axios from 'axios';
+
+const baseUrl = 'http://sidar-staging.suryoatmojo.my.id';
+
 var product = [
   {backColor: '#2b2b2b', category: 'Kopi', brand: 'Ucafe', rasa: 'mocachinno'},
   {backColor: '#2b2b2b', category: 'Kopi', brand: 'Rasa Sayang', rasa: 'kopi'},
@@ -40,24 +44,60 @@ var product = [
   {backColor: '#2b2b2b', category: 'Kopi', brand: 'rasa sayang', rasa: 'kopi'},
 ];
 
+// var divisi = [];
+
 var cobavar = 'suryo';
 
 class LaporanDar extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      dar: [],
+      color: '',
+    };
   }
 
-  //   componentDidMount() {
-  //     this.unsubsribe = this.props.navigation.addListener('focus', () => {
-  //       console.log('hello world');
-  //       //ambild data di server bisa dilakukan disini
-  //     });
-  //   }
+  componentDidMount() {
+    this.unsubsribe = this.props.navigation.addListener('focus', () => {
+      // console.log(this.state.username);
+      // console.log(this.state.password);
+      // let username = 'suryo';
+      // let password = '123456';
+      console.log('ini did mount laporan dar');
 
-  //   componentWillUnmount() {
-  //     this.unsubscribe();
-  //   }
+      //ambild data di server bisa dilakukan disini
+      axios({
+        method: 'get',
+        url: `${baseUrl}/api/sidar_dar/all`,
+        headers: {
+          'X-Api-Key': '0ED40DE05125623C8753B6D3196C18DE',
+          'X-Token':
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImlkIjoiMSJ9LCJpYXQiOjE2NTg0NTcxMTMsImV4cCI6MTY1ODU0MzUxM30.4EY-P_2X9NHe7ZvCWc6pttCYRsegVDHivkHKXK9Kyhs',
+        },
+      })
+        .then(response => {
+          console.log('api get');
+          console.log(response.status);
+          console.log(response.data.data.sidar_dar);
+          this.setState({dar: response.data.data.sidar_dar});
+          console.log('ini isi divisi');
+          console.log(this.state.dar);
+
+          // console.log(response.data.data[0]);
+          // console.log(response.data.message);
+          if (response.data.message == 'success') {
+            console.log(response.data.data[0].username);
+          }
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubsribe();
+  }
 
   render() {
     return (
@@ -88,13 +128,13 @@ class LaporanDar extends Component {
         {/* <Text>Pergi Ke Detail</Text> */}
         <FlatList
           style={{marginTop: 10}}
-          data={product}
+          data={this.state.dar}
           renderItem={({item, index}) => (
             //styling view
             <TouchableOpacity
               style={{
                 // backgroundColor: '#60a5f0',
-                backgroundColor: item.backColor,
+                backgroundColor: '#2b2b2b',
                 marginTop: 10,
                 marginHorizontal: 10,
                 padding: 20,
@@ -104,9 +144,86 @@ class LaporanDar extends Component {
                 borderBottomRightRadius: 5,
               }}>
               {/* <View style={{flex: 1}}></View> */}
+
+              {item.status == 'ontime' ? (
+                <Text
+                  style={{
+                    backgroundColor: 'green',
+                    borderRadius: 50,
+                    paddingHorizontal: 5,
+                    width: 160,
+                    paddingVertical: 5,
+                    marginBottom: 20,
+                    fontWeight: 'bold',
+                    fontSize: 10,
+                    color: '#ffffff',
+                  }}>
+                  {item.id} / Suryo Atmojo
+                </Text>
+              ) : item.status == 'late' ? (
+                <Text
+                  style={{
+                    backgroundColor: 'yellow',
+                    borderRadius: 50,
+                    paddingHorizontal: 5,
+                    width: 160,
+                    paddingVertical: 5,
+                    marginBottom: 20,
+                    fontWeight: 'bold',
+                    fontSize: 10,
+                    color: '#ffffff',
+                  }}>
+                  {item.id} / Suryo Atmojo
+                </Text>
+              ) : item.status == 'over' ? (
+                <Text
+                  style={{
+                    backgroundColor: 'red',
+                    borderRadius: 50,
+                    paddingHorizontal: 5,
+                    width: 160,
+                    paddingVertical: 5,
+                    marginBottom: 20,
+                    fontWeight: 'bold',
+                    fontSize: 10,
+                    color: '#ffffff',
+                  }}>
+                  {item.id} / Suryo Atmojo
+                </Text>
+              ) : item.status == 'absence' ? (
+                <Text
+                  style={{
+                    backgroundColor: 'blue',
+                    borderRadius: 50,
+                    paddingHorizontal: 5,
+                    width: 160,
+                    paddingVertical: 5,
+                    marginBottom: 20,
+                    fontWeight: 'bold',
+                    fontSize: 10,
+                    color: '#ffffff',
+                  }}>
+                  {item.id} / Suryo Atmojo
+                </Text>
+              ) : (
+                <Text
+                  style={{
+                    backgroundColor: 'black',
+                    borderRadius: 50,
+                    paddingHorizontal: 5,
+                    width: 160,
+                    paddingVertical: 5,
+                    marginBottom: 20,
+                    fontWeight: 'bold',
+                    fontSize: 10,
+                    color: '#ffffff',
+                  }}>
+                  {item.id} / Suryo Atmojo
+                </Text>
+              )}
               <Text
                 style={{
-                  backgroundColor: 'rgba(0,0,0,0.3)',
+                  backgroundColor: this.state.color,
                   borderRadius: 50,
                   paddingHorizontal: 5,
                   width: 160,
@@ -116,7 +233,7 @@ class LaporanDar extends Component {
                   fontSize: 10,
                   color: '#ffffff',
                 }}>
-                {item.category}
+                {item.id} / Suryo Atmojo
               </Text>
               <View style={{flexDirection: 'row'}}>
                 <View style={{flex: 1}}>
@@ -124,15 +241,15 @@ class LaporanDar extends Component {
                     style={{
                       // marginLeft: 5,
                       fontWeight: 'bold',
-                      fontSize: 22,
+                      fontSize: 20,
                       color: '#ffffff',
                     }}>
-                    {item.brand}
+                    {item.tanggal} / {item.jam}
                   </Text>
-                  <Text style={{color: '#aad5fc'}}>{item.rasa}</Text>
+                  <Text style={{color: '#aad5fc'}}>{item.status}</Text>
                 </View>
                 <View>
-                  <Icon name="laptop" size={50} color="rgba(255,255,255,0.5)" />
+                  <Icon name="check" size={50} color="rgba(255,255,255,0.5)" />
                 </View>
               </View>
             </TouchableOpacity>

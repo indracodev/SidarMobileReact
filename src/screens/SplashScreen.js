b9c6e07d76c1128a10207c72243adc0a7f7868cf
suryo <suryoatm@gmail.com>
2022-07-27 13:41:57 +0700

    insert : author label

--- src/screens/SplashScreen.js
+++ src/screens/SplashScreen.js
@@ -1,3 +1,18 @@
+/**
+ * For the brave souls who get this far: You are the chosen ones,
+ * the valiant knights of programming who toil away, without rest,
+ * fixing our most awful code. To you, true saviors, kings of men,
+ * I say this: never gonna give you up, never gonna let you down,
+ * never gonna run around and desert you. Never gonna make you cry,
+ * never gonna say goodbye. Never gonna tell a lie and hurt you.
+ */
+
+/**
+ * author : Suryo Atmojo <suryoatm@gmail.com>
+ * project : INDRACO-SIDAR
+ * Start-date : 23-07-2022
+ */
+
 import React, {Component} from 'react';
 import {Text, View, Image} from 'react-native';
 import {StackActions} from '@react-navigation/native';
@@ -11,7 +26,8 @@ class SplashScreen extends Component {
     // console.log('component did mount gaes');
     setTimeout(() => {
       //   this.props.navigation.navigate('Home');
-      this.props.navigation.dispatch(StackActions.replace('SignIn'));
+      // this.props.navigation.dispatch(StackActions.replace('SignIn'));
+      this.props.navigation.dispatch(StackActions.replace('Login'));
     }, 2500);
   };
 

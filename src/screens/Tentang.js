ba887e60aed84fe7fd323ca56a6efc7b083d500e
suryo <suryoatm@gmail.com>
2022-07-28 12:10:35 +0700

    created : tentang screen

---
+++ src/screens/Tentang.js
@@ -0,0 +1,211 @@
+/**
+ * For the brave souls who get this far: You are the chosen ones,
+ * the valiant knights of programming who toil away, without rest,
+ * fixing our most awful code. To you, true saviors, kings of men,
+ * I say this: never gonna give you up, never gonna let you down,
+ * never gonna run around and desert you. Never gonna make you cry,
+ * never gonna say goodbye. Never gonna tell a lie and hurt you.
+ */
+
+/**
+ * author : Suryo Atmojo <suryoatm@gmail.com>
+ * project : INDRACO-SIDAR
+ * Start-date : 23-07-2022
+ */
+
+import React, {Component} from 'react';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  TextInput,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
+import {StackActions} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import SignInHeader from '../components/SignInHeader';
+import TextArea from '../components/TextArea';
+import axios from 'axios';
+
+const baseUrl = 'http://sidar-staging.suryoatmojo.my.id';
+// const baseUrl = 'http://localhost/sidar-new';
+class Dar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  componentWillUnmount() {}
+
+  render() {
+    return (
+      <View style={{backgroundColor: '#373737', flex: 1}}>
+        {/* <View style={{flex: 1}}> */}
+        <View
+          style={{
+            borderBottomRightRadius: 20,
+            borderBottomLeftRadius: 20,
+            backgroundColor: '#393939',
+            padding: 20,
+          }}>
+          <TouchableOpacity onPress={this.toggleOpen}>
+            <Icon name="cog" size={30} color="#ffffff" />
+            {/* <Text
+                    style={{
+                      color: '#000000',
+                      fontsize: 9,
+                    }}>
+                    gear
+                  </Text> */}
+          </TouchableOpacity>
+          <Text
+            style={{
+              color: '#ffffff',
+              fontSize: 25,
+              fontWeight: 'bold',
+              marginTop: 10,
+            }}>
+            INDRACO - SIDAR
+          </Text>
+          {/* <Text style={{color: '#ffffff', fontSize: 12}}>DAR</Text> */}
+        </View>
+        {/* </View> */}
+
+        <View
+          style={{
+            marginTop: 10,
+            marginBottom: 5,
+            backgroundColor: '#2b2b2b',
+            borderTopRightRadius: 12,
+            borderTopLeftRadius: 12,
+            borderBottomRightRadius: 12,
+            borderBottomLeftRadius: 12,
+          }}>
+          <Text
+            style={{
+              fontSize: 22,
+              fontWeight: 'bold',
+              color: '#ffffff',
+              textAlign: 'center',
+              marginTop: 10,
+            }}>
+            Tentang
+          </Text>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 16,
+            }}></Text>
+        </View>
+
+        {/* <TextArea placeholder="Description" /> */}
+
+        <ScrollView style={{flexDirection: 'column', marginBottom: 20}}>
+          <View
+            style={{
+              backgroundColor: '#2b2b2b',
+              padding: 25,
+              marginLeft: 5,
+              marginRight: 5,
+              marginBottom: 20,
+            }}>
+            <TextInput
+              style={styles.textArea}
+              underlineColorAndroid="transparent"
+              placeholder="Aktifitas Harian"
+              placeholderTextColor="white"
+              numberOfLines={10}
+              multiline={true}
+              onChangeText={text => this.setState({activity: text})}
+            />
+          </View>
+        </ScrollView>
+
+        <TouchableOpacity
+          style={{
+            marginBottom: 40,
+            backgroundColor: '#272727',
+            paddingVertical: 15,
+            marginHorizontal: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 9,
+            elevation: 2,
+          }}
+          onPress={this.submitData}>
+          <Text style={{color: '#FFFFFF', fontSize: 18, fontWeight: 'light'}}>
+            Simpan
+          </Text>
+        </TouchableOpacity>
+
+        <View
+          style={{
+            backgroundColor: '#2b2b2b',
+            flexDirection: 'row',
+            paddingVertical: 10,
+            borderTopRightRadius: 12,
+            borderTopLeftRadius: 12,
+          }}>
+          <TouchableOpacity
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+            onPress={() => this.props.navigation.navigate('LaporanDar')}>
+            <Icon name="chart-bar" size={20} color="#ffffff" />
+            <Text
+              style={{
+                color: '#ffffff',
+                fontsize: 9,
+              }}>
+              Laporan
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+            onPress={() => this.props.navigation.navigate('Home')}>
+            <Icon name="home" size={25} color="#ffffff" />
+            <Text
+              style={{
+                color: '#ffffff',
+                fontsize: 9,
+              }}>
+              Home
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+            onPress={() => this.props.navigation.navigate('Dar')}>
+            <Icon name="book" size={20} color="#ffffff" />
+            <Text
+              style={{
+                color: '#ffffff',
+                fontsize: 9,
+              }}>
+              DAR
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  textAreaContainer: {
+    borderColor: '#c5c5c5',
+    backgroundColor: '#000000',
+    borderWidth: 1,
+    padding: 5,
+  },
+  textArea: {
+    height: 150,
+    justifyContent: 'flex-start',
+    color: '#ffffff',
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
+});
+
+export default Dar;

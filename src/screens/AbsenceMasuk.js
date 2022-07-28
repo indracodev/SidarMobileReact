35d540f5f164970cc9947dd8a721bc61455f54a8
suryo <suryoatm@gmail.com>
2022-07-28 22:17:53 +0700

    modified : open file location

--- src/screens/AbsenceMasuk.js
+++ src/screens/AbsenceMasuk.js
@@ -1,122 +1,58 @@
-// React Native Geolocation
-// https://aboutreact.com/react-native-geolocation/
+/**
+ * For the brave souls who get this far: You are the chosen ones,
+ * the valiant knights of programming who toil away, without rest,
+ * fixing our most awful code. To you, true saviors, kings of men,
+ * I say this: never gonna give you up, never gonna let you down,
+ * never gonna run around and desert you. Never gonna make you cry,
+ * never gonna say goodbye. Never gonna tell a lie and hurt you.
+ */
 
-// import React in our code
-import React, {useState, useEffect} from 'react';
-import Icon from 'react-native-vector-icons/FontAwesome5';
+/**
+ * author : Suryo Atmojo <suryoatm@gmail.com>
+ * project : INDRACO-SIDAR
+ * Start-date : 23-07-2022
+ */
 
-// import all the components we are going to use
+import React, {Component} from 'react';
 import {
   SafeAreaView,
-  View,
   Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  TextInput,
   StyleSheet,
+  ScrollView,
   Image,
   PermissionsAndroid,
-  ScrollView,
   Platform,
   Button,
-  TouchableOpacity,
   StatusBar,
 } from 'react-native';
-
-//import all the components we are going to use.
-import Geolocation from '@react-native-community/geolocation';
+import {StackActions} from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import SignInHeader from '../components/SignInHeader';
+import TextArea from '../components/TextArea';
+import axios from 'axios';
 import DocumentPicker from 'react-native-document-picker';
 
-const AbsenceMasuk = () => {
-  const [currentLongitude, setCurrentLongitude] = useState('...');
-  const [currentLatitude, setCurrentLatitude] = useState('...');
-  const [locationStatus, setLocationStatus] = useState('');
+const baseUrl = 'http://sidar-staging.suryoatmojo.my.id';
+// const baseUrl = 'http://localhost/sidar-new';
+class AbsenceMasuk extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      deskripsi: 'ajur gaes ajur 2',
+      file: 'content://com.android.providers.media.documents/document/document%3A17',
+    };
+  }
 
-  // useEffect(() => {
-  //   const requestLocationPermission = async () => {
-  //     if (Platform.OS === 'ios') {
-  //       getOneTimeLocation();
-  //       subscribeLocationLocation();
-  //     } else {
-  //       try {
-  //         const granted = await PermissionsAndroid.request(
-  //           PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-  //           {
-  //             title: 'Location Access Required',
-  //             message: 'This App needs to Access your location',
-  //           },
-  //         );
-  //         if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-  //           //To Check, If Permission is granted
-  //           getOneTimeLocation();
-  //           subscribeLocationLocation();
-  //         } else {
-  //           setLocationStatus('Permission Denied');
-  //         }
-  //       } catch (err) {
-  //         console.warn(err);
-  //       }
-  //     }
-  //   };
-  //   requestLocationPermission();
-  //   return () => {
-  //     Geolocation.clearWatch(watchID);
-  //   };
-  // }, []);
+  componentDidMount() {}
 
-  // const getOneTimeLocation = () => {
-  //   setLocationStatus('Refresh Location');
-  //   Geolocation.getCurrentPosition(
-  //     //Will give you the current location
-  //     position => {
-  //       setLocationStatus('You are Here');
+  componentWillUnmount() {}
 
-  //       //getting the Longitude from the location json
-  //       const currentLongitude = position.coords.longitude;
-
-  //       //getting the Latitude from the location json
-  //       const currentLatitude = position.coords.latitude;
-
-  //       //Setting Longitude state
-  //       setCurrentLongitude(currentLongitude);
-
-  //       //Setting Longitude state
-  //       setCurrentLatitude(currentLatitude);
-  //     },
-  //     error => {
-  //       setLocationStatus(error.message);
-  //     },
-  //     {
-  //       enableHighAccuracy: false,
-  //       timeout: 30000,
-  //       maximumAge: 1000,
-  //     },
-  //   );
-  // };
-
-  // const subscribeLocationLocation = () => {
-  //   watchID = Geolocation.watchPosition(
-  //     position => {
-  //       //Will give you the location on location change
-  //       setLocationStatus('You are Here');
-  //       console.log(position);
-  //       //getting the Longitude from the location json
-  //       const currentLongitude = JSON.stringify(position.coords.longitude);
-  //       //getting the Latitude from the location json
-  //       const currentLatitude = JSON.stringify(position.coords.latitude);
-  //       //Setting Longitude state
-  //       setCurrentLongitude(currentLongitude);
-  //       //Setting Latitude state
-  //       setCurrentLatitude(currentLatitude);
-  //     },
-  //     error => {
-  //       setLocationStatus(error.message);
-  //     },
-  //     {
-  //       enableHighAccuracy: false,
-  //       maximumAge: 1000,
-  //     },
-  //   );
-  // };
-
-  const selectOneFile = async () => {
+  selectOneFile = async () => {
     var uri;
     var type;
     var name;
@@ -136,6 +72,8 @@ const AbsenceMasuk = () => {
       console.log(res[0]);
       console.log(res[0].name);
       console.log(res[0].uri);
+
+      this.setState({file: res[0].uri});
       //Printing the log realted to the file
       // console.log('res : ' + JSON.stringify(res, null, 2));
       // console.log('URI : ' + res[0].uri);
@@ -143,7 +81,7 @@ const AbsenceMasuk = () => {
       // console.log('File Name : ' + res.name);
       // console.log('File Size : ' + res.size);
       //Setting the state to show single file attributes
-      setSingleFile(res);
+      // setSingleFile(res);
     } catch (err) {
       //Handling any exception (If any)
       if (DocumentPicker.isCancel(err)) {
@@ -157,165 +95,244 @@ const AbsenceMasuk = () => {
     }
   };
 
-  const selectMultipleFile = async () => {
-    //Opening Document Picker for selection of multiple file
-    try {
-      const results = await DocumentPicker.pickMultiple({
-        type: [DocumentPicker.types.images],
-        //There can me more options as well find above
+  submitData = () => {
+    console.log('tombol simpan mengirimkan data');
+    console.log('token');
+    // console.log(this.props.route.params.token);
+
+    // let username = this.state.username;
+    // let password = this.state.password;
+    // bodyFormData.append('username', 'suryoatm');
+    // bodyFormData.append('password', '085649224822');
+
+    var bodyFormData = new FormData();
+    bodyFormData.append('deskripsi', this.state.deskripsi);
+    bodyFormData.append('file', this.state.file);
+
+    axios({
+      method: 'post',
+      url: `${baseUrl}/api/sidar_kritik/add`,
+      data: bodyFormData,
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'X-Api-Key': '0ED40DE05125623C8753B6D3196C18DE',
+        'X-Token':
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImlkIjoiMSJ9LCJpYXQiOjE2NTg5ODM2MjAsImV4cCI6MTY1OTA3MDAyMH0.FOYwvJXZ7vkOUjsJZuvy66xT51IgRRYxKcyMN9rRMB8',
+      },
+    })
+      .then(response => {
+        // console.log(response.data.status);
+        // console.log(response.data.data.username);
+        // console.log(response.data.token);
+        // console.log(this.state.username);
+        // console.log(this.state.password);
+        if (response.data.status == true) {
+          this.props.navigation.dispatch(
+            StackActions.replace('LaporanDar', {
+              // data: response.data.data,
+              // token: response.data.token,
+            }),
+          );
+        } else {
+          alert('periksa kembali inputan  anda');
+        }
+      })
+      .catch(function (err) {
+        console.log(err);
+        alert('periksa kembali inputan anda');
       });
-      for (const res of results) {
-        //Printing the log realted to the file
-        console.log('res : ' + JSON.stringify(res));
-        console.log('URI : ' + res.uri);
-        console.log('Type : ' + res.type);
-        console.log('File Name : ' + res.name);
-        console.log('File Size : ' + res.size);
-      }
-      //Setting the state to show multiple file attributes
-      setMultipleFile(results);
-    } catch (err) {
-      //Handling any exception (If any)
-      if (DocumentPicker.isCancel(err)) {
-        //If user canceled the document selection
-        alert('Canceled from multiple doc picker');
-      } else {
-        //For Unknown Error
-        alert('Unknown Error: ' + JSON.stringify(err));
-        throw err;
-      }
-    }
   };
 
-  const [singleFile, setSingleFile] = useState('');
-  const [multipleFile, setMultipleFile] = useState([]);
+  render() {
+    return (
+      <View style={{backgroundColor: '#373737', flex: 1}}>
+        {/* <View style={{flex: 1}}> */}
+        <View
+          style={{
+            borderBottomRightRadius: 20,
+            borderBottomLeftRadius: 20,
+            backgroundColor: '#393939',
+            padding: 20,
+          }}>
+          <TouchableOpacity onPress={this.toggleOpen}>
+            <Icon name="cog" size={30} color="#ffffff" />
+            {/* <Text
+                    style={{
+                      color: '#000000',
+                      fontsize: 9,
+                    }}>
+                    gear
+                  </Text> */}
+          </TouchableOpacity>
+          <Text
+            style={{
+              color: '#ffffff',
+              fontSize: 25,
+              fontWeight: 'bold',
+              marginTop: 10,
+            }}>
+            INDRACO - SIDAR
+          </Text>
+          {/* <Text style={{color: '#ffffff', fontSize: 12}}>DAR</Text> */}
+        </View>
+        {/* </View> */}
 
-  return (
-    <View style={{flex: 1}}>
-      <View style={styles.container}>
-        {/* <Image
+        <View
+          style={{
+            marginTop: 10,
+            marginBottom: 5,
+            backgroundColor: '#2b2b2b',
+            borderTopRightRadius: 12,
+            borderTopLeftRadius: 12,
+            borderBottomRightRadius: 12,
+            borderBottomLeftRadius: 12,
+          }}>
+          <Text
+            style={{
+              fontSize: 22,
+              fontWeight: 'bold',
+              color: '#ffffff',
+              textAlign: 'center',
+              marginTop: 10,
+            }}>
+            Laporan Aktivitas Harian
+          </Text>
+          <Text
+            style={{
+              textAlign: 'center',
+              fontSize: 16,
+            }}></Text>
+        </View>
+
+        {/* <TextArea placeholder="Description" /> */}
+
+        <ScrollView style={{flexDirection: 'column', marginBottom: 20}}>
+          <View style={styles.container}>
+            {/* <Image
             source={{
               uri: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/location.png',
             }}
             style={{width: 100, height: 100}}
           /> */}
-        <Text>ABSENCE MASUK</Text>
-        <Text style={styles.boldText}>{locationStatus}</Text>
+            <Text>ABSENCE MASUK</Text>
+            {/* <Text style={styles.boldText}>{locationStatus}</Text> */}
 
-        <SafeAreaView style={{flex: 1}}>
-          <Text style={styles.titleText}>
-            Example of File Picker in React Native
-          </Text>
-          <View style={styles.container}>
-            {/*To show single file attribute*/}
-            <TouchableOpacity
-              activeOpacity={0.5}
-              style={styles.buttonStyle}
-              onPress={selectOneFile}>
-              {/*Single file selection button*/}
-              <Text style={{marginRight: 10, fontSize: 19}}>
-                Click here to pick one file
+            <SafeAreaView style={{flex: 1}}>
+              <Text style={styles.titleText}>
+                Example of File Picker in React Native
               </Text>
-              <Image
-                source={{
-                  uri: 'https://img.icons8.com/offices/40/000000/attach.png',
-                }}
-                style={styles.imageIconStyle}
-              />
-            </TouchableOpacity>
-            {/*Showing the data of selected Single file*/}
-            <Text style={styles.textStyle}>
-              File Name: {singleFile.name ? singleFile.name : ''}
-              {'\n'}
-              Type: {singleFile.type ? singleFile.type : ''}
-              {'\n'}
-              File Size: {singleFile.size ? singleFile.size : ''}
-              {'\n'}
-              URI: {singleFile.uri ? singleFile.uri : ''}
-              {'\n'}
-            </Text>
-            <View
-              style={{
-                backgroundColor: 'grey',
-                height: 2,
-                margin: 10,
-              }}
-            />
-            {/*To multiple single file attribute*/}
-            <TouchableOpacity
-              activeOpacity={0.5}
-              style={styles.buttonStyle}
-              onPress={selectMultipleFile}>
-              {/*Multiple files selection button*/}
-              <Text style={{marginRight: 10, fontSize: 19}}>
-                Click here to pick multiple files
-              </Text>
-              <Image
-                source={{
-                  uri: 'https://img.icons8.com/offices/40/000000/attach.png',
-                }}
-                style={styles.imageIconStyle}
-              />
-            </TouchableOpacity>
-            <ScrollView>
-              {/*Showing the data of selected Multiple files*/}
-              {multipleFile.map((item, key) => (
-                <View key={key}>
-                  <Text style={styles.textStyle}>
-                    File Name: {item.name ? item.name : ''}
-                    {'\n'}
-                    Type: {item.type ? item.type : ''}
-                    {'\n'}
-                    File Size: {item.size ? item.size : ''}
-                    {'\n'}
-                    URI: {item.uri ? item.uri : ''}
-                    {'\n'}
+              <View style={styles.container}>
+                {/*To show single file attribute*/}
+                <TouchableOpacity
+                  activeOpacity={0.5}
+                  style={styles.buttonStyle}
+                  onPress={this.selectOneFile}>
+                  {/*Single file selection button*/}
+                  <Text style={{marginRight: 10, fontSize: 19}}>
+                    Click here to pick one file
                   </Text>
-                </View>
-              ))}
-            </ScrollView>
+                  <Image
+                    source={{
+                      uri: 'https://img.icons8.com/offices/40/000000/attach.png',
+                    }}
+                    style={styles.imageIconStyle}
+                  />
+                </TouchableOpacity>
+                {/*Showing the data of selected Single file*/}
+
+                <View
+                  style={{
+                    backgroundColor: 'grey',
+                    height: 2,
+                    margin: 10,
+                  }}
+                />
+                {/*To multiple single file attribute*/}
+              </View>
+            </SafeAreaView>
           </View>
-        </SafeAreaView>
+        </ScrollView>
+
+        <TouchableOpacity
+          style={{
+            marginBottom: 40,
+            backgroundColor: '#272727',
+            paddingVertical: 15,
+            marginHorizontal: 20,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 9,
+            elevation: 2,
+          }}
+          onPress={this.submitData}>
+          <Text style={{color: '#FFFFFF', fontSize: 18, fontWeight: 'light'}}>
+            Simpan
+          </Text>
+        </TouchableOpacity>
+
+        <View
+          style={{
+            backgroundColor: '#2b2b2b',
+            flexDirection: 'row',
+            paddingVertical: 10,
+            borderTopRightRadius: 12,
+            borderTopLeftRadius: 12,
+          }}>
+          <TouchableOpacity
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+            onPress={() => this.props.navigation.navigate('LaporanDar')}>
+            <Icon name="chart-bar" size={20} color="#ffffff" />
+            <Text
+              style={{
+                color: '#ffffff',
+                fontsize: 9,
+              }}>
+              Laporan
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+            onPress={() => this.props.navigation.navigate('Home')}>
+            <Icon name="home" size={25} color="#ffffff" />
+            <Text
+              style={{
+                color: '#ffffff',
+                fontsize: 9,
+              }}>
+              Home
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}
+            onPress={() => this.props.navigation.navigate('Dar')}>
+            <Icon name="book" size={20} color="#ffffff" />
+            <Text
+              style={{
+                color: '#ffffff',
+                fontsize: 9,
+              }}>
+              DAR
+            </Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
-  );
-};
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#373737',
-    padding: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  boldText: {
-    fontSize: 30,
-    color: 'red',
-    marginVertical: 16,
-  },
-  titleText: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    paddingVertical: 20,
-  },
-  textStyle: {
-    backgroundColor: '#fff',
-    fontSize: 15,
-    marginTop: 16,
-    color: 'black',
-  },
-  buttonStyle: {
-    alignItems: 'center',
-    flexDirection: 'row',
-    backgroundColor: '#DDDDDD',
+  textAreaContainer: {
+    borderColor: '#c5c5c5',
+    backgroundColor: '#000000',
+    borderWidth: 1,
     padding: 5,
   },
-  imageIconStyle: {
-    height: 20,
-    width: 20,
-    resizeMode: 'stretch',
+  textArea: {
+    height: 150,
+    justifyContent: 'flex-start',
+    color: '#ffffff',
+    fontSize: 15,
+    fontWeight: 'bold',
   },
 });
 

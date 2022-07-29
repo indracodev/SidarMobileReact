929f2d4023aa8c56d3b7b7b576f8605d98ae2158
suryo <suryoatm@gmail.com>
2022-07-29 16:53:11 +0700

    modified : integration camera and file explore

--- src/screens/AbsenceMasuk.js
+++ src/screens/AbsenceMasuk.js
@@ -13,7 +13,7 @@
  * Start-date : 23-07-2022
  */
 
-import React, {Component} from 'react';
+import React, {Fragment, Component} from 'react';
 import {
   SafeAreaView,
   Text,
@@ -28,14 +28,34 @@ import {
   Platform,
   Button,
   StatusBar,
+  Dimensions,
 } from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
 import {StackActions} from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import SignInHeader from '../components/SignInHeader';
 import TextArea from '../components/TextArea';
 import axios from 'axios';
 import DocumentPicker from 'react-native-document-picker';
-
+import * as ImagePicker from 'react-native-image-picker';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+// import * as ImagePicker from 'react-native-image-picker';
+const options = {
+  title: 'Select Avatar',
+  customButtons: [{name: 'fb', title: 'Choose Photo from Facebook'}],
+  storageOptions: {
+    skipBackup: true,
+    path: 'images',
+  },
+};
 const baseUrl = 'http://sidar-staging.suryoatmojo.my.id';
 // const baseUrl = 'http://localhost/sidar-new';
 class AbsenceMasuk extends Component {
@@ -43,9 +63,169 @@ class AbsenceMasuk extends Component {
     super(props);
     this.state = {
       id: '',
-      deskripsi: 'ajur gaes ajur 2',
-      file: 'content://com.android.providers.media.documents/document/document%3A17',
+      deskripsi: 'serangan fajar qwerty',
+      file: 'file:///data/user/0/com.sidarmobilereact/cache/rn_image_picker_lib_temp_5e66fc3d-58a6-4b64-bf3a-1ae43179bb03.jpg',
+      uri: '',
+      type: '',
+      name: '',
+      filepath: {
+        data: '',
+        uri: '',
+      },
+      filePath: '',
+      fileData: '',
+      fileUri: '',
     };
+  }
+
+  chooseImage = () => {
+    // alert('Canceled from single doc picker');
+    launchImageLibrary(
+      {
+        skipBackup: true,
+        path: 'images',
+      },
+      response => {
+        console.log('Response = ', response);
+
+        if (response.didCancel) {
+          console.log('User cancelled image picker');
+        } else if (response.error) {
+          console.log('ImagePicker Error: ', response.error);
+        } else if (response.customButton) {
+          console.log('User tapped custom button: ', response.customButton);
+          alert(response.customButton);
+        } else {
+          const source = {uri: response.uri};
+          console.log('test disini');
+          console.log(response.assets[0]);
+          console.log(response.assets[0].uri);
+          // You can also display the image using data:
+          // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+          // alert(JSON.stringify(response));s
+          console.log('response', JSON.stringify(response));
+          this.setState({
+            name: response.assets[0].fileName,
+            type: response.assets[0].type,
+            uri: response.assets[0].uri,
+            filePath: response,
+            fileData: response.assets[0],
+            fileUri: response.assets[0].uri,
+          });
+        }
+      },
+    );
+
+    // console.log(test);
+
+    // You can also use as a promise without 'callback':
+    // const result = await launchImageLibrary({
+    //         skipBackup: true,
+    //         path: 'images',
+    //       });
+  };
+
+  lCamera = () => {
+    // const test = launchCamera({
+    //   skipBackup: true,
+    //   path: 'images',
+    // });
+
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    // alert('haaiiii');
+    launchCamera(options, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        const source = {uri: response.uri};
+
+        console.log('test disini');
+        console.log(response.assets[0]);
+        console.log(response.assets[0].uri);
+
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          name: response.assets[0].fileName,
+          type: response.assets[0].type,
+          uri: response.assets[0].uri,
+          filePath: response,
+          fileData: response.assets[0],
+          fileUri: response.assets[0].uri,
+        });
+      }
+    });
+  };
+
+  launchImageLibrary = () => {
+    let options = {
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    ImagePicker.launchImageLibrary(options, response => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+        alert(response.customButton);
+      } else {
+        const source = {uri: response.uri};
+        console.log('response', JSON.stringify(response));
+        this.setState({
+          filePath: response,
+          fileData: response.data,
+          fileUri: response.uri,
+        });
+      }
+    });
+  };
+
+  renderFileData() {
+    if (this.state.fileData) {
+      return (
+        <Image
+          source={{uri: 'data:image/jpeg;base64,' + this.state.fileData}}
+          style={styles.images}
+        />
+      );
+    } else {
+      return (
+        <Image
+          source={require('../../assets/dummy.png')}
+          style={styles.images}
+        />
+      );
+    }
+  }
+
+  renderFileUri() {
+    if (this.state.fileUri) {
+      return <Image source={{uri: this.state.fileUri}} style={styles.images} />;
+    } else {
+      return (
+        <Image
+          source={require('../../assets/galeryImages.jpg')}
+          style={styles.images}
+        />
+      );
+    }
   }
 
   componentDidMount() {}
@@ -72,8 +252,19 @@ class AbsenceMasuk extends Component {
       console.log(res[0]);
       console.log(res[0].name);
       console.log(res[0].uri);
+      console.log(res[0].type);
+      const name = decodeURIComponent(res[0].uri);
+      // if (name.startsWith(CONTENT_PREFIXES.RESILLIO_SYNC)) {
+      //   const realPath = name.replace(CONTENT_PREFIXES.RESILLIO_SYNC, '');
+      // }
+      console.log(name);
+      // console.log(realPath);
 
-      this.setState({file: res[0].uri});
+      this.setState({file: name});
+      this.setState({uri: res[0].uri});
+      this.setState({name: res[0].name});
+      this.setState({size: res[0].size});
+
       //Printing the log realted to the file
       // console.log('res : ' + JSON.stringify(res, null, 2));
       // console.log('URI : ' + res[0].uri);
@@ -98,6 +289,7 @@ class AbsenceMasuk extends Component {
   submitData = () => {
     console.log('tombol simpan mengirimkan data');
     console.log('token');
+    console.log(this.state.fileData);
     // console.log(this.props.route.params.token);
 
     // let username = this.state.username;
@@ -106,8 +298,22 @@ class AbsenceMasuk extends Component {
     // bodyFormData.append('password', '085649224822');
 
     var bodyFormData = new FormData();
-    bodyFormData.append('deskripsi', this.state.deskripsi);
-    bodyFormData.append('file', this.state.file);
+    bodyFormData.append('deskripsi', 'yoggi');
+    bodyFormData.append('file', {
+      // name: 'file',
+      name: this.state.name,
+      type: 'image/jpeg',
+      uri: this.state.uri,
+    });
+    // bodyFormData.append('file', this.state.fileData);
+    console.log('name');
+    console.log(this.state.name);
+    console.log('type');
+    console.log(this.state.type);
+    console.log('uri');
+    console.log(this.state.uri);
+    console.log('file');
+    console.log(bodyFormData._parts[1]);
 
     axios({
       method: 'post',
@@ -117,7 +323,7 @@ class AbsenceMasuk extends Component {
         'Content-Type': 'multipart/form-data',
         'X-Api-Key': '0ED40DE05125623C8753B6D3196C18DE',
         'X-Token':
-          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImlkIjoiMSJ9LCJpYXQiOjE2NTg5ODM2MjAsImV4cCI6MTY1OTA3MDAyMH0.FOYwvJXZ7vkOUjsJZuvy66xT51IgRRYxKcyMN9rRMB8',
+          'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRhIjp7ImlkIjoiMSJ9LCJpYXQiOjE2NTkwNjQwNjEsImV4cCI6MTY1OTE1MDQ2MX0.350MvthNRDpcGZnYoayZoORSS7G0pityYCw-nes7xKQ',
       },
     })
       .then(response => {
@@ -127,12 +333,7 @@ class AbsenceMasuk extends Component {
         // console.log(this.state.username);
         // console.log(this.state.password);
         if (response.data.status == true) {
-          this.props.navigation.dispatch(
-            StackActions.replace('LaporanDar', {
-              // data: response.data.data,
-              // token: response.data.token,
-            }),
-          );
+          alert('berhasil');
         } else {
           alert('periksa kembali inputan  anda');
         }
@@ -147,6 +348,7 @@ class AbsenceMasuk extends Component {
     return (
       <View style={{backgroundColor: '#373737', flex: 1}}>
         {/* <View style={{flex: 1}}> */}
+
         <View
           style={{
             borderBottomRightRadius: 20,
@@ -187,6 +389,11 @@ class AbsenceMasuk extends Component {
             borderBottomRightRadius: 12,
             borderBottomLeftRadius: 12,
           }}>
+          {/* <Image
+            source={uri(
+              'content://com.android.providers.media.documents/document/image:307420',
+            )}
+          /> */}
           <Text
             style={{
               fontSize: 22,
@@ -207,6 +414,47 @@ class AbsenceMasuk extends Component {
         {/* <TextArea placeholder="Description" /> */}
 
         <ScrollView style={{flexDirection: 'column', marginBottom: 20}}>
+          <SafeAreaView>
+            <View style={styles.body}>
+              <Text
+                style={{textAlign: 'center', fontSize: 20, paddingBottom: 10}}>
+                Pick Images from Camera & Gallery
+              </Text>
+              <View style={[styles.ImageSections, {alignItems: 'center'}]}>
+                <View>
+                  {this.renderFileData()}
+                  <Text style={{textAlign: 'center'}}>Base 64 String</Text>
+                </View>
+                <View>
+                  {this.renderFileUri()}
+                  <Text style={{textAlign: 'center'}}>File Uri</Text>
+                </View>
+              </View>
+
+              <View style={styles.btnParentSection}>
+                <TouchableOpacity
+                  onPress={this.chooseImage}
+                  style={styles.btnSection}>
+                  <Text style={styles.btnText}>Choose File</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  onPress={this.lCamera}
+                  style={styles.btnSection}>
+                  <Text style={styles.btnText}>Directly Launch Camera</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                  onPress={this.launchImageLibrary}
+                  style={styles.btnSection}>
+                  <Text style={styles.btnText}>
+                    Directly Launch Image Library
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </SafeAreaView>
+
           <View style={styles.container}>
             {/* <Image
             source={{
@@ -217,7 +465,7 @@ class AbsenceMasuk extends Component {
             <Text>ABSENCE MASUK</Text>
             {/* <Text style={styles.boldText}>{locationStatus}</Text> */}
 
-            <SafeAreaView style={{flex: 1}}>
+            <SafeAreaView style={{flex: 1, flexDirection: 'column'}}>
               <Text style={styles.titleText}>
                 Example of File Picker in React Native
               </Text>
@@ -321,17 +569,49 @@ class AbsenceMasuk extends Component {
 }
 
 const styles = StyleSheet.create({
-  textAreaContainer: {
-    borderColor: '#c5c5c5',
-    backgroundColor: '#000000',
-    borderWidth: 1,
-    padding: 5,
+  scrollView: {
+    backgroundColor: Colors.lighter,
   },
-  textArea: {
+
+  body: {
+    backgroundColor: Colors.white,
+    justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    // height: Dimensions.get('screen').height - 20,
+    // width: Dimensions.get('screen').width,
+  },
+  ImageSections: {
+    display: 'flex',
+    flexDirection: 'column',
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    justifyContent: 'center',
+  },
+  images: {
+    width: 150,
     height: 150,
-    justifyContent: 'flex-start',
-    color: '#ffffff',
-    fontSize: 15,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginHorizontal: 3,
+  },
+  btnParentSection: {
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  btnSection: {
+    width: 225,
+    height: 50,
+    backgroundColor: '#DCDCDC',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+    marginBottom: 10,
+  },
+  btnText: {
+    textAlign: 'center',
+    color: 'gray',
+    fontSize: 14,
     fontWeight: 'bold',
   },
 });
